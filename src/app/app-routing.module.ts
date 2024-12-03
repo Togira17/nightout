04fae95548539496bab7196833eaa94e2a3c81f7
Yit-hub <>
2024-12-03 14:13:00 +0100

    inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'header', component: HeaderComponent},
-  { path: 'footer', component: FooterComponent}
+  { path: 'footer', component: FooterComponent},
+  { path: 'inicio', component: InicioComponent}
 
 ];
 
