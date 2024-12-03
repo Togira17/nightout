afa4ae370b69565e228e8d6b6cc915b70011fdc0
manuel <manuelgonzalezcastillero@gmail.com>
2024-12-03 20:33:03 +0100

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  //inicializamos variables
+  isFixed = false;
+  headerInitialOffset = 0; 
 
+  ngOnInit(): void {
+    // inicializamos constantes
+    const header = document.querySelector('.header') as HTMLElement;
+    this.headerInitialOffset = header.offsetTop; 
+  }
+
+  @HostListener('window:scroll', [])
+  //funcion se realiza al hacer scroll y calcula
+  onWindowScroll() {
+    const scrollPosition = window.scrollY
+    this.isFixed = scrollPosition > this.headerInitialOffset;
+  }
 }
+
