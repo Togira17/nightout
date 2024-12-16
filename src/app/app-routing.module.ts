d14f7fb4d792d4281999f5ce239d9cbd41a30290
Jaime Guerrero <163987047+LemanSachs29@users.noreply.github.com>
2024-12-16 11:20:27 +0100

    commit
    añadido el routing del carrito. Arreglado para que el inicio no aparezca fuera del componente principal.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'header', component: HeaderComponent},
   { path: 'footer', component: FooterComponent},
+  { path: 'carrito', component: CarritoComponent}
 ];
 
 @NgModule({
