3e9336a41390d5fb7621acf94722514fe6f7ebe6
Javier Villaseñor García <163987047+LemanSachs29@users.noreply.github.com>
2025-02-12 10:24:03 +0100

    hecho el registro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthServiceService } from '../auth.service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  user =
+    {
+      "email": '', //Debe ser único
+      "password": '',
+      "nombre_usuario": '', //Debe ser único
+      "titular_tarjeta": '',
+      "numero_tarjeta_credito": '',
+      "cvc": '',
+      "dni": '12345678Y', //la longitud debe ser <= 9
+      "first_name": '',
+      "last_name": '',
+    };
+
+
+  credentials =
+    {
+      "email": '',
+      "password": ''
+    };
+
+  constructor(private authService: AuthServiceService) { }
+
+
+  register() {
+
+    this.authService.register(this.user).subscribe({
+      next: (response) => console.log('Registro exitoso', response),
+      error: (err) => console.error('Error en el registro', err)
+    });
+  }
+
+  login() {
+    this.authService.login(this.credentials).subscribe({
+      next: (response) => console.log('Login exitoso', response),
+      error: (err) => console.error('Error en el login', err)
+    });
+  }
+
 
 }
