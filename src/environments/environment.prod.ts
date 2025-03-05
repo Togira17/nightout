2cbdccd43630983ec833d6204238df59bf321fdc
Juan Blanco <163987047+LemanSachs29@users.noreply.github.com>
2025-03-05 14:30:27 +0100

    f

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8055',
-    backendUrl: 'http://localhost/api/pedido.php',
+    apiUrl: 'https://apidirectus.duckdns.org',
+    backendUrl: 'https://mibackend.duckdns.org/api/pedido.php',
 };
